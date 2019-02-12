3be1a8a8b369766f1673bd14e17470afdb2961f1
Alexa Fonseca <alexafdeveloper@gmail.com>
2019-02-12 19:43:48 +0000

    12vn

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,75 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
- *print_times_table  -  prints the n times table, starting with 0
- *@n: is a int
- *Return: values 0
+ *print_one  -  prints numbers less 9
+ *@a: is a int
+ *@b: is a int
+ *Declaration: Auxilio
+ *Return: nothing
  */
+void print_one(int a, int b)
+{
+	if (b != 0)
+	{
+		_putchar(' ');
+		_putchar(' ');
+	}
+	_putchar(a + '0');
+}
+/**
+  *print_two - prints numbers > 9 && < 99
+  *@c: is a int
+  *Return: nothing
+  */
+void print_two(int c)
+{
+	_putchar(' ');
+	_putchar((c / 10) + '0');
+	_putchar((c % 10) + '0');
+}
+/**
+  *print_three - print number >99
+  *@d : is a int
+  *Return: Nothing
+  */
+void print_three(int d)
+{
+	_putchar((d / 100) + '0');
+	_putchar(((d / 10) % 10) + '0');
+	_putchar((d % 10) + '0');
+}
+/**
+  *print_times_table  -  prints the n times table, starting with 0
+  *@n: is a int
+  *Return: values 0
+  */
 void print_times_table(int n)
 {
-	int a, b, c;
+	int e, f, g;
 
 	if (n > 0 && n < 15)
 	{
-		for (a = 0 ; a <= n ; a++)
+		for (e = 0 ; e <= n ; e++)
 		{
-			for (b = 0 ; b <= n ; b++)
+			for (f = 0 ; f <= n ; f++)
 			{
-				c = a * b;
-				if (b != 0)
+				g = e * f;
+				if (g > 99)
+					print_three(g);
+				if (g >= 10 && g <= 99)
+					print_two(g);
+				if (g <= 9)
+					print_one(g, f);
+				if (f != n)
 				{
-					if (c <= 9)
-					{
-						printf("  ");
-					}
-					if (c > 9 && c <= 99)
-					{
-						printf(" ");
-					}
+					_putchar(',');
+					_putchar(' ');
 				}
-				printf("%d", c);
-				if (b != n)
+				else
 				{
-					printf(", ");
+					_putchar('\n');
 				}
 			}
-			printf("\n");
 		}
-		printf("\n");
 	}
 }
