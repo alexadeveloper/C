1bad84477a6fd74a8417e2f4cb43863356b325fa
Alexa Fonseca <alexafdeveloper@gmail.com>
2019-02-12 15:07:39 +0000

    9v2

--- 0x02-functions_nested_loops/9-times_table_bad.c
+++ 0x02-functions_nested_loops/9-times_table_bad.c
@@ -1,30 +1,23 @@
 #include "holberton.h"
 
 /**
- *times_table -  prints the last digit of a number
- *
- *Return: Nothin
+  *times_table -  prints the 9 times table, starting with 0
+  *
+  *Return: Nothing
  */
 void times_table(void)
 {
 	int h, m, a;
-
 	for (h = 0; h <= 9; h++)
 	{
 		for (m = 0; m <= 9; m++)
 		{
 			a = (h * m);
-			if (a > 9)
+			_putchar(a + '0');
+			if (m == 9)
 			{
-				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
 			}
-			else 
-			{
-				_putchar(' ');
-				_putchar(a + '0');
-			}
-			if (m != 9)
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
