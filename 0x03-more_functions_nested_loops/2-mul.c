9bd54a9e4aa9b1508ba478e24f5d1aad8250080f
Alexa <alexafdeveloper@gmail.com>
2019-02-14 17:58:03 +0000

    2

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul -  multiplies two integers.
+ * @a: is a int
+ * @b: is a int
+ * Return: The result
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -13,4 +13,5 @@
 int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
