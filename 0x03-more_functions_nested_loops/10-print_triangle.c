0360bbc4d7696b4ccb471d3d877277bcb23921cc
Alexa <alexafdeveloper@gmail.com>
2019-02-14 21:18:59 +0000

    10

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+*print_triangle - draws a triangle in the terminal
+*@size: is a int
+*Return: values 0
+*/
+void print_triangle(int size)
+{
+	int times, espacios, imprime;
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (times = 1; times <= size; times++)
+		{
+			for (espacios = size; espacios > times; espacios--)
+			{
+				_putchar(' ');
+			}
+			for (imprime = 1; imprime <= times; imprime++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
