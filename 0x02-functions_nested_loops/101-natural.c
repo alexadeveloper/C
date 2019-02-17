5ed81906a797efd855b30e14b21b57692f54649c
Alexa <alexafdeveloper@gmail.com>
2019-02-17 23:56:42 +0000

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,5 +7,17 @@
  */
 int main(void)
 {
+	int n, num, sum;
+
+	num = 1024;
+	for (n = 0; n < num; n++)
+	{
+		if (n % 3 == 0 ||  n % 5 == 0)
+		{
+			sum = sum + n;
+		}
+	}
+	printf("%d", sum);
+	putchar('\n');
 return (0);
 }
