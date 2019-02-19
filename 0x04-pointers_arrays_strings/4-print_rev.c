6334c7ce040807610e206b8b74f1bacc22677d4a
Alexa <alexafdeveloper@gmail.com>
2019-02-19 15:37:20 +0000

    4

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+  *print_rev -print reverse a string
+  *@s: is a char
+ */
+
+void print_rev(char *s)
+{
+	int cont, i;
+	char letra;
+
+	cont = 0;
+	while (s[cont] != '\0')
+	{
+		cont++;
+	}
+	for (i = cont; i >= 0; i--)
+	{
+		letra = s[i];
+		_putchar(letra);
+	}
+	_putchar('\n');
+}
