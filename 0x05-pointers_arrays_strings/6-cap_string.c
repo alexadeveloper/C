cdffd1af641bbe7917f8eabbe70426dc34280fd0
Alexa <alexafdeveloper@gmail.com>
2019-02-21 22:02:14 +0000

    6v2

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,27 @@
 char *cap_string(char *s)
 {
 	int i, j, k;
+	int separadores[] = { 9, 10, 32, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 || s[i] == '\t' || s[i] == '\n' || s[i] == ' ' || s[i] == '.')
+		if (i == 0 && (s[i] >= 97 && s[i] <= 122))
 		{
-			if (i == 0)
+			j = s[i] - 32;
+			s[i] = j;
+		}
+		else
+		{
+			for (k = 0; k <= 12; k++)
 			{
-				k = i;
-			}
-			else
-			{
-				k = i + 1;
-			}
-			if (s[k] >= 97 && s[k] <= 122)
-			{
-				j = s[k] - 32;
-				s[k] = j;
+				if (s[i] == separadores[k])
+				{
+					if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					{
+						j = s[i + 1] - 32;
+						s[i + 1] = j;
+					}
+				}
 			}
 		}
 	}
