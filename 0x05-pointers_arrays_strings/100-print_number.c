6ebfbb4c686d4f2ae98f4be63211cf1a3929b2fe
Alexa <alexafdeveloper@gmail.com>
2019-02-22 21:57:08 +0000

    10

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -2,71 +2,38 @@
 #include <stdio.h>
 
 /**
-  *mypow -do a power
-  *@base: is a int
-  *@power: is a exponent
-  *Return: the pow
+  *print_number -print an integer
+  *@s: is a integer
  */
 
-int mypow(int base, int power)
+void print_number(int n)
 {
-	int p, potencia;
+	int i, j, num, cont;
+	int divisores[] = { 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1 };
 
-	potencia = 1;
-	if (power == 0)
-	{
-		potencia = 1;
-	}
-	else
-	{
-		for (p = 1; p <= power; p++)
-		{
-			potencia = potencia * base;
-		}
-	}
-return (potencia);
-}
-/**
-  *_atoi -convert a string to integer
-  *@s: is a char
-  *Return: the num
- */
-
-int _atoi(char *s)
-{
-	int i, j, cont, signo, suma, num, power;
-
+	num = n;
 	cont = 0;
-	suma = 0;
-	power = 0;
-	signo = 1;
-	for (i = 0; s[i] != '\0'; i++)
+	if (num < 0)
 	{
-		if (s[i] == 42 && cont == 0)
-			signo = signo * (1);
-		if (s[i] == 45 && cont == 0)
-			signo = signo * (-1);
-		if (s[i] >= 48 && s[i] <= 57)
-			cont++;
-		if ((s[i] < 48 || s[i] > 57) && cont > 0)
-			break;
+		_putchar('-');
+		num = num * -1;
 	}
-	if (cont == 0)
+	if (num == 0)
 	{
-	return (0);
+		_putchar('0');
 	}
-	else
+	for (i = 0; i <= 7; i++)
 	{
-		for (j = i; j >= 0; j--)
+		j = num / divisores[i];
+		if (j > 0)
 		{
-			if (s[j] >= 48 && s[j] <= 57)
-			{
-				num = s[j] - 48;
-				suma = suma + (num * mypow(10, power));
-				power++;
-			}
-			suma = suma * signo;
+			_putchar(j + 48);
+			cont++;
 		}
-	return (suma);
+		num = num - (j * divisores[i]);
+		if ((num / divisores[i + 1]) == 0 && cont != 0 && i != 7)
+		{
+			_putchar('0');
+		}
 	}
 }
