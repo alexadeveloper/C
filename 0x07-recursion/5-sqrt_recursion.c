d6fe14cda6770ea968bc2f8cd23c9909e970a884
Alexa <alexafdeveloper@gmail.com>
2019-02-27 19:22:45 +0000

    5

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,5 +1,27 @@
 #include "holberton.h"
+#include <stdio.h>
 
+/**
+  *validar - test the root
+  *@n: is a int
+  *@o: is a int
+  *Return: the root square
+ */
+
+int validar(int n, int o)
+{
+
+	if ((o * o) > n)
+	{
+		return (-1);
+	}
+	if ((float) n / o != o)
+	{
+		return (validar(n, (o + 1)));
+	}
+	else
+	return (o);
+}
 /**
   *_sqrt_recursion - square root of a number
   *@n: is the number
@@ -8,13 +30,13 @@
 
 int _sqrt_recursion(int n)
 {
-	if (y == 0)
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
 	{
 		return (1);
 	}
-	if (y < 0)
-	{
-		return (-1);
-	}
-return (x * _pow_recursion(x, y - 1));
+	return (validar(n, 2));
 }
