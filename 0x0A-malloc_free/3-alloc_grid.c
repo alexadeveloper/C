b4dac690480207b51447659f7455528d55092833
Alexa <alexafdeveloper@gmail.com>
2019-03-06 00:38:30 +0000

    3v2

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,7 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+  *_strlen - returns the length of a string
+  *@s: is a char
+  *Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int cont;
+
+	cont = 0;
+	while (s[cont] != '\0')
+	{
+		cont++;
+	}
+return (cont);
+}
 
 /**
  *alloc_grid- return a array 2 dimensional
