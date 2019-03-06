16c001200d1ca89c7b885543c65cf078272710a3
Alexa <alexafdeveloper@gmail.com>
2019-03-06 02:58:32 +0000

    5v1

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 
 /**
+  *_strlen - returns the length of a string
+  *@s: is a char
+  *Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int cont;
+
+	cont = 0;
+	while (s[cont] != '\0')
+	{
+		cont++;
+	}
+return (cont);
+}
+/**
   *argstostr- args to string
   *@ac: cantidad de argumentos
   *@av: argumentos pasados
@@ -11,21 +27,39 @@
 
 char *argstostr(int ac, char **av)
 {
-	unsigned int i;
-	char *str;
+	unsigned int i, j, k, l, size, largo;
+	char *str, *cad;
 
 	if (ac == 0 || av == NULL)
 	{
 	return (NULL);
 	}
-	str = malloc(sizeof(char) * (ac));
+	largo = 0;
+	for (i = 1; av[i] != '\0'; i++)
+	{
+		size = _strlen(av[i]);
+		largo = largo + size;
+	}
+	str = malloc(sizeof(char) * (largo + 1));
 	if (str == NULL)
 	{
 	return (NULL);
 	}
-	for (i = 1; av[i] != '\0'; i++)
+	j = 0;
+	while (j < largo)
 	{
-		printf("%s\n", av[i]);
+		for (k = 1; k < i; k++)
+		{
+			size = _strlen(av[k]);
+			cad = av[k];
+			for (l = 0; l < size; l++)
+			{
+				str[j] = cad[l];
+				j++;
+			}
+			str[j] = '\n';
+			j++;
+		}
 	}
 return (str);
 }
