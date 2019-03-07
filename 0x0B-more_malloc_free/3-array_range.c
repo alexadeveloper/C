2b21ce4fb11674faff1f470d9dfee8c568a2b553
Alexa <alexafdeveloper@gmail.com>
2019-03-07 18:34:02 +0000

    3v0

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ *array_range - allocates memory for an array
+ *@min: is size of array
+ *@max: size of type
+ *Return: null it is fail
+ */
+
+int *array_range(int min, int max)
+{
+	int *matrix;
+	unsigned int i, tam;
+
+	if (min > max)
+	{
+	return (NULL);
+	}
+	tam = max - min;
+	matrix = malloc(sizeof(int) * (tam + 1));
+	if (matrix == NULL)
+	{
+		return (NULL);
+	}
+	for (i = min; i <= tam; i++)
+	{
+		matrix[i] = i;
+	}
+return (matrix);
+}
