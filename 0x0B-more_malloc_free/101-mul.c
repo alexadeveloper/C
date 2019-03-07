a03703a257f28259867f9eb88842399d68ee3b03
Alexa <alexafdeveloper@gmail.com>
2019-03-07 21:46:17 +0000

    5v1

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -2,13 +2,29 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-#include "holberton.h"
+/**
+  *_strlen - returns the length of a string
+  *@s: is a char
+  *Return: the length of a string
+  */
+int _strlen(char *s)
+{
+	int cont;
+
+	cont = 0;
+	while (s[cont] != '\0')
+	{
+		cont++;
+	}
+return (cont);
+}
+
 /**
   * _isdigit -  checks for a digit.
   * @c: is a int
   * Return: Always 0 (Success)
  */
-int _isdigit(int c)
+int _isdigit(char c)
 {
 	return (c >= 48 && c <= 57 ? 1 : 0);
 }
@@ -23,16 +39,33 @@ int _isdigit(int c)
 int main(int argc, char *argv[])
 {
 	unsigned int resultado;
-	unsigned int a, b;
+	unsigned int a, b, i, conta, contb, aux;
+	char *newa, *newb;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 	exit(98);
 	}
-	a = _isdigit(atoi(argv[1]));
-	b = _isdigit(atoi(argv[2]));
-	if (a == 0 || b == 0)
+	a = _strlen(argv[1]);
+	newa = argv[1];
+	aux = 0;
+	for (i = 0; i < a; i++)
+	{
+		aux = _isdigit(newa[i]);
+		if (aux == 0)
+			conta++;
+	}
+	b = _strlen(argv[2]);
+	aux = 0;
+	newb = argv[2];
+	for (i = 0; i < b; i++)
+	{
+		aux = _isdigit(newb[i]);
+		if (aux == 0)
+			contb++;
+	}
+	if (conta != 0 || contb != 0)
 	{
 		printf("Error\n");
 	exit(98);
@@ -40,5 +73,6 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 	resultado = a * b;
-return (resultado);
+	printf("%d\n", resultado);
+return (0);
 }
