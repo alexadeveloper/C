ea64ab43c399fb13e4455da9864fd6f90a858953
Alexa <alexafdeveloper@gmail.com>
2019-03-11 17:18:26 +0000

    inicial

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - struct for the object in c
+  * @name: pointer
+  * @age: float
+  * @owner: char*
+  *
+  * Description: Struct of my dog in c
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
