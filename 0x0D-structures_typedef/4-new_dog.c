fa27b6402c3957eaf4615c17683bb154e73f5420
Alexa <alexafdeveloper@gmail.com>
2019-03-12 00:46:05 +0000

    4v2

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -30,6 +30,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *copy_name, *copy_owner;
 	dog_t *new;
+	unsigned int tam_name, tam_owner;
 
 	if (name == NULL)
 		return (NULL);
@@ -38,7 +39,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
+	tam_name = _strlen(name);
+	copy_name = malloc(sizeof(char) * tam_name);
+	if (copy_name == NULL)
+	{
+		free(copy_name);
+		return (NULL);
+	}
 	copy_name = name;
+	tam_owner = _strlen(owner);
+	copy_owner = malloc(sizeof(char) * tam_owner);
+	if (copy_owner == NULL)
+	{
+		free(copy_owner);
+		return (NULL);
+	}
 	copy_owner = owner;
 	(*new).name = copy_name;
 	(*new).age = age;
