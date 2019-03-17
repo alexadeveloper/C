086f17163fcd20b8399229cffa30ea5c7db5fff5
alexadeveloper <47119661+alexadeveloper@users.noreply.github.com>
2019-03-16 20:09:34 -0500

    fixed: \n

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -6,7 +6,7 @@ main:				;tell linker entry point
 	    mov     edx,len	;message length
 	    mov     ecx,msg	;message to write
 	    mov     ebx,1	;file descriptor (stdout)
-	    mov     eax,4	;system call number (sys_write)
+	    mov     eax,17	;system call number (sys_write)
 	    int     0x80	;call kernel
 
 	    mov     eax,1	;system call number (sys_exit)
@@ -14,5 +14,5 @@ main:				;tell linker entry point
 
 	section     .data
 
-	msg     db  'Hello, Holberton',0xa	;our dear string
+	msg     db  'Hello, Holberton\n',0xa	;our dear string
 	len     equ $ - msg		;length of our dear string
