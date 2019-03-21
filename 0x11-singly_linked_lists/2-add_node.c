1f2ec6b2521761d8b464100a24b6add7e8e0042a
Alexa <alexafdeveloper@gmail.com>
2019-03-21 17:55:33 +0000

    fixed: strlen own

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+/**
+  *_strlen - returns the length of a string
+  *@s: is a char
+  *Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int cont;
+	cont = 0;
+	while (s[cont] != '\0')
+	{
+		cont++;
+	}
+return (cont);
+}
+
 /**
   *add_node - adds a new node at the beginning of a list_t
   *@head: pointer to pointers
@@ -14,7 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	copy = malloc(sizeof(list_t));
 	copy->str = strdup(str);
-	copy->len = strlen(copy->str);
+	copy->len = _strlen(copy->str);
 	copy->next = *head;
 	*head = copy;
 return (*head);
