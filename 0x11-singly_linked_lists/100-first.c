ee7bc902a36a5d17d23915eb6dab816b0c291741
Alexa <alexafdeveloper@gmail.com>
2019-03-21 21:25:06 +0000

    feature: prints before the function executed

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  *print_before - print a string before the main
+ */
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
