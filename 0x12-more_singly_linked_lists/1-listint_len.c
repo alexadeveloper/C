43889426610acbcd6cf415e0a3da648d2cee7d86
Alexa <alexafdeveloper@gmail.com>
2019-03-25 14:49:19 +0000

    fixed: betty style & added: point 1

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -2,18 +2,23 @@
 #include <stdio.h>
 
 /**
- *print_listint - print a list
+ *listint_len - number of elements in a linked list
  *@h: is a pointer with the list
  *Return: the num objects
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nodes;
+	unsigned int largo = 0;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
 	for (nodes = 0; h != NULL; nodes++)
 	{
-		printf("%d\n", h->n);
+		largo++;
 		h = h->next;
 	}
-return (nodes);
+return (largo);
 }
