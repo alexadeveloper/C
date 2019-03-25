3706f682b90c021251adad3dcbdc437ca9d9e3cf
Alexa <alexafdeveloper@gmail.com>
2019-03-25 17:37:42 +0000

    feature: return the sum of all the data of a list

---
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *sum_listint - returns the sum of all the data of a list
+ *@head: is a pointer with the list
+ *Return: the num of the node
+ */
+int sum_listint(listint_t *head)
+{
+	size_t nodes;
+	unsigned int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	for (nodes = 0; head != NULL; nodes++)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+return (sum);
+}
