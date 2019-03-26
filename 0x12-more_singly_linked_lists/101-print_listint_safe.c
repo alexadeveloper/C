fe4c7d358cffca0703ce3317d181e1b5b0aa9724
Alexa <alexafdeveloper@gmail.com>
2019-03-26 22:24:24 +0000

    feature: prints a linked list with a circle

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_listint_safe - print a list int
+ *@head: pointer of pointer of the list
+ *Return: the num of the node
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int nodes, anteriores;
+	const listint_t *aux, *new;
+
+	aux = head;
+	for (nodes = 0; aux != NULL; nodes++)
+	{
+		printf("[%p] %d\n", (void *)aux,  aux->n);
+		aux = aux ->next;
+		new = head;
+		for (anteriores = 0; anteriores < nodes; anteriores++)
+		{
+			if (aux == new)
+			{
+				 printf("[%p] %d\n", (void *)aux,  aux->n);
+				 return (nodes);
+			}
+			new = new->next;
+		}
+	}
+return (nodes);
+}
