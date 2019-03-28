c1918683f944aff369dce59abd4be3161b1f0f18
Alexa <alexafdeveloper@gmail.com>
2019-03-28 19:16:31 +0000

    fixed: correct checker 8 -11

--- 0x13-bit_manipulation/test/1-print_binary.c
+++ 0x13-bit_manipulation/test/1-print_binary.c
@@ -7,25 +7,15 @@ void print_binary(unsigned long int n)
 {
 	unsigned int bit = 0;
 	unsigned long int mask;
-	int control = 0;
 
 	mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
 	while (bit < (sizeof(unsigned long int) * 8))
 	{
-		if ((n & mask) != 1)
-			control = 1;
-		if (control == 1)
-		{
-			if ((n & mask) == 0)
-				_putchar('0');
-			else
-				_putchar('1');
-		}
+		if ((n & mask) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
 		mask = mask >> 1;
 		bit++;
-	}
-	if (control == 0)
-	{
-		_putchar('0');
 	}
 }
