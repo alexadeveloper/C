d01a3b3d5741ef4d163093752c67ef2065799066
Alexa <alexafdeveloper@gmail.com>
2019-03-28 20:07:25 +0000

    fixed: betty style

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
   *get_bit - returns the value of a bit a given index
   *@n: the number send it
   *@index: the bit you want to get
-  *Returns: The value of the bit, or -1 it is fails
+  *Return: The value of the bit, or -1 it is fails
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
