3faf1c473984ced4b35311ecac341a636582b3ad
Alexa <alexafdeveloper@gmail.com>
2019-03-28 20:03:38 +0000

    feature: returns the value of a bit at a given index

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  *get_bit - returns the value of a bit a given index
+  *@n: the number send it
+  *@index: the bit you want to get
+  *Returns: The value of the bit, or -1 it is fails
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(n) * 8))
+		return (-1);
+return ((n >> index) & 1);
+}
