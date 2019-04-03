8e24902fd3b2f66477569dcb8f99a21c87bcf22b
Alexa <alexafdeveloper@gmail.com>
2019-04-03 17:03:59 +0000

    feature: text null return 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(abrir);
-	return (-1);
+	return (1);
 	}
 	count = 0;
 	for (i = 0; text_content[i] != 0; i++)
