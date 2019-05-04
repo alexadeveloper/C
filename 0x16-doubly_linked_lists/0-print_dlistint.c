13f80163c9e919edfcb1d7223d915e08c2c76d42
Alexa <alexafdeveloper@gmail.com>
2019-05-04 17:04:41 +0000

    fixes: betty

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
 /**
-  *print_dlistint- print all the elements of a dlistint_t 
+  *print_dlistint- print all the elements of a dlistint_t
   *@h: is a pointer with the list
   *Return: The number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes;
-	
+
 	if (h == NULL)
 		return (0);
 	for (nodes = 0; h != NULL; nodes++)
