6ed0c77494a24704179af096c5afdfdb0f5c860c
Alexa <alexafdeveloper@gmail.com>
2019-05-05 18:08:42 +0000

    fixes: if *head is NULL

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -11,13 +11,24 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *copy;
 
+	if (head == NULL)
+		return (NULL);
 	copy = malloc(sizeof(dlistint_t));
 	if (copy == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 	copy->n = n;
-	copy->next = *head;
+	if (*head == NULL)
+	{
+		copy->next = NULL;
+		copy->prev = NULL;
+	}
+	else
+	{
+		copy->next = *head;
+		copy->prev = NULL;
+	}
 	*head = copy;
-return (*head);
+return (copy);
 }
