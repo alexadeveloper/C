8c73b2aac159249ef37ea1cb476d4270a2bbdb9d
Alexa <alexafdeveloper@gmail.com>
2019-05-31 20:57:03 +0000

    fixes: free

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -9,19 +9,19 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int cont;
 	hash_node_t *aux;
 
-	if (!ht)
+	if (ht == NULL)
 	{
 		return;
 	}
 	for  (cont = 0; cont < ht->size; cont++)
 	{
-		while (!ht->array)
+		while (ht->array[cont] != NULL)
 		{
 			aux = ht->array[cont];
-			free(aux->key);
 			free(aux->value);
-			ht->array[cont] = ht->array[cont]->next;
+			free(aux->key);
 			free(aux);
+			ht->array[cont] = ht->array[cont]->next;
 		}
 	}
 	free(ht->array);
