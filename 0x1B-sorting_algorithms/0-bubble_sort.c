6e661e0b3c3c8ccfe7eafe0b685100f9b9f356c5
Alexa <alexafdeveloper@gmail.com>
2019-06-19 09:35:39 +0000

    bubble sort

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+  *bubble_sort- sorting algoritm qith bubble
+  *@array: the input to organize
+  *@size: the size of the arrar
+  *Return: void
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, n;
+	
+	if (array == NULL || size == 0 || size == 1)
+	{
+		return;
+	}
+	for(i = 0; i < (size - 1); i++)
+	{
+		for(j = 0; j < (size - i - 1); j++)
+		{
+			if(array[j] > array[j + 1])
+			{
+				n = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = n;
+				print_array(array, size);
+			}
+		}
+	}
+}
