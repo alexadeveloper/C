30ca5db4f8b61b34050430fe0b5b232a07a4f590
Alexa <alexafdeveloper@gmail.com>
2019-07-05 20:30:46 +0000

    task 5 checkers 8, 9, 10, 12

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,10 +9,13 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 1 or grid[i][j - 1] == 1:
-                    lados = 2
-                else:
-                    lados = 4
+                lados = 4
+                if i != len(grid) - 1:
+                    if grid[i + 1][j]:
+                        lados = 2
+                if j != len(grid[i]) - 1:
+                    if grid[i][j + 1] == 1:
+                        lados = 2
             else:
                 lados = 0
             perimetro = perimetro + lados
