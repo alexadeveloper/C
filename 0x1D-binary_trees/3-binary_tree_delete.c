429e4b320ef32f651e5c559258fec7de23542504
Alexa <alexafdeveloper@gmail.com>
2019-07-23 17:55:36 +0000

    adding: deletes an entire binary tree

---
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_delete-  delete a tree
+ *@tree: pointer to the node root
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		free(tree);
+		return;
+	}
+	if (tree->right != NULL)
+               binary_tree_delete(tree->right);
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	free(tree);
+}
